d71f5d62f22e83ad0496211a947956081bace98f
takehiro1111 <t.soccer.0916@gmail.com>
2024-07-28 23:09:07 +0900

    SSMパラメータストアの参照を定義

--- sam/waf_rule/function/slack_notify.py
+++ sam/waf_rule/function/slack_notify.py
@@ -1,7 +1,28 @@
-import requests    
+import requests
+import boto3    
+
+ssm = boto3.client('ssm','ap-northeast-1')
+
+def get_ssm_parameter()-> str:
+    try:
+        response = ssm.get_parameter(
+            Name='/common/SLACK_WEBHOOK',
+            WithDecryption=True
+        )
+        return response['Parameter']['Value']
+    
+    except ssm.exceptions.ParameterNotFound:
+        raise ValueError("SSM parameter not found.")
+    
+    except ssm.exceptions.ParameterVersionNotFound:
+        raise ValueError("SSM parameter version not found.")
+    
+    except Exception as e:
+        raise ValueError(f"Failed to get SSM parameter: {e}")
+
 
 def send_slack_notification(web_acl_name, rule_name, status):
-    webhook_url = ''
+    webhook_url = get_ssm_parameter()
     recreate_message = {
         'text': f'Success WebACL:{web_acl_name} / Rule:{rule_name} / Status: {status}'
     }
