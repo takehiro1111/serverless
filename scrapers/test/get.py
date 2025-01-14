9fc06da376150aeb93aaaf94ffb551169949259d
takehiro1111 <t.soccer.0916@gmail.com>
2025-01-15 00:11:32 +0900

    スクレイピングテスト

---
+++ scrapers/test/get.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# HTMLの取得(GET)
+req = requests.get("html/sampleGet.html")
+req.encoding = req.apparent_encoding # 日本語の文字化け防止
+
+# HTMLの解析
+bsObj = BeautifulSoup(req.text,"html.parser")
+
+# 要素の抽出
+items = bsObj.find_all("li")
+item = items.find("a").get("href")
+
+
+if __name__ == "__main__":
+  print(item)
