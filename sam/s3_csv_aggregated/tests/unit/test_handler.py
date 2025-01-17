fb1e478c572d45e057e7f8d6a497c17496cc084c
takehiro1111 <t.soccer.0916@gmail.com>
2025-01-18 00:22:25 +0900

    add:samプロジェクトの作成

--- sam/s3_csv_aggregated/tests/unit/test_handler.py
+++ sam/s3_csv_aggregated/tests/unit/test_handler.py
@@ -1,13 +1,12 @@
 import json
 
 import pytest
-
 from function import app
 
 
 @pytest.fixture()
 def apigw_event():
-    """ Generates API GW Event"""
+    """Generates API GW Event"""
 
     return {
         "body": '{ "test": "body"}',
