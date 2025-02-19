f679cb306e197948bdcf31a7d517109b9ae5bd43
takehiro1111 <t.soccer.0916@gmail.com>
2025-02-20 00:25:49 +0900

    update:クォータ制限の回避

--- sam/monitor_ecs_capacity_provider/function/check_ecs.py
+++ sam/monitor_ecs_capacity_provider/function/check_ecs.py
@@ -137,7 +137,7 @@ def check_capacity_provider(
     try:
         # サービス名リストを10個ずつのバッチに分割する関数
         def chunk_list(lst, chunk_size=10):
-            return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+            return [lst[i : i + chunk_size] for i in range(0, len(lst), chunk_size)]
 
         # 個々のECSサービス(stg)でキャパシティプロバイダを確認する。
         # もし、FARGATEのものがあればそのサービス名を出力する。
