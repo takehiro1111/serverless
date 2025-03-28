beccaa1e7cdb7ce6442d3c1f21149c3f9a9586ab
Takehiro Tanaka <t.soccer.0916@gmail.com>
2025-03-28 12:00:24 +0900

    add:test

--- local/github/main.py
+++ local/github/main.py
@@ -7,8 +7,8 @@ import requests
 
 # GithubAppのinstallationIDを取得する処理。
 # GitHub App設定
-app_id = "1234567"
-private_key_path = "hoge.pem"
+app_id = "273743"
+private_key_path = "private_key.pem"
 
 # 秘密鍵を読み込む
 with open(private_key_path) as f:
@@ -20,8 +20,10 @@ def create_jwt():
     now = int(time.time())
     payload = {"iat": now, "exp": now + (10 * 60), "iss": app_id}  # 10分間有効
     token = jwt.encode(payload, private_key, algorithm="RS256")
+    print("token", token)
     if isinstance(token, bytes):
         token = token.decode("utf-8")  # Python 3.5 以前の場合必要
+
     return token
 
 
